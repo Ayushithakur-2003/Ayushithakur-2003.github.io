1fdfb199fc78e38b21fe0c2770f89927cff305de
Ayushithakur <thakurayushi696@gmail.com>
2023-04-03 18:22:23 +0530

    setup gh-pages

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -38,7 +38,7 @@ render(){
         </div>
         <div style={{...this.styleIt}} class="text-wrapper">
   <p style={{...this.paraStyle}} >
-    Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
+    Lorem ipsum odor, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
     dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
     Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
     sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
